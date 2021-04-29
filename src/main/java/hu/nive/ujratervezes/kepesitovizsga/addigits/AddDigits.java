1bf085f01b5df4a467e51d9992dd6eeb9a50bdef
hex-twentyfive <czinky.gyorgy@gmail.com>
2021-04-29 10:34:12 +0200

    1st task completed

---
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/addigits/AddDigits.java
@@ -0,0 +1,33 @@
+package hu.nive.ujratervezes.kepesitovizsga.addigits;
+
+public class AddDigits {
+
+
+    public int addDigits(String input) {
+
+        if (input == null || input.equals("")) {
+            return (-1);
+        }
+
+        String filtered = filterString(input);
+
+        if(filtered.equals("")) {
+            return 0;
+        }
+
+        String[] parts = filtered.split("");
+
+        int digitSum = 0;
+        for(String e: parts) {
+            digitSum += Integer.parseInt(e);
+        }
+
+        return digitSum;
+    }
+
+    private String filterString(String input) {
+        String filteredString = input.replaceAll("[\\D]","");
+        return filteredString;
+    }
+
+}
